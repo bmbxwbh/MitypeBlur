b6616e1387785f47497a38f2ab13b5b131c0f241
bmbxwbh <bmbxwbh@users.noreply.github.com>
2026-09-25 14:07:42 +0800

    chore: bump version to v2.1.22 (232)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mitype.blur"
         minSdk = 26
         targetSdk = 35
-        versionCode = 231
-        versionName = "2.1.21"
+        versionCode = 232
+        versionName = "2.1.22"
     }
 
     buildTypes {
